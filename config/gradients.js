f2f7907225edb55074c139ad04d15e44b9b8cac4
Jason Nedell <jason@nedell.com>
2015-06-17 01:30:55 -0600

    Add some gradient presets; Separate out different particle events;

--- config/gradients.js
+++ config/gradients.js
@@ -1,11 +1,58 @@
-cymk = [
+cyanMagentaYellow = [
   { color: '#00FFFF', location: 0 },
-  { color: '#FF00FF', location: 0.5 },
-  { color: '#FFFF00', location: 1 },
+  { color: '#FF00FF', location: 0.25 },
+  { color: '#FFFF00', location: 0.5 },
+  { color: '#FF00FF', location: 0.75 },
+  { color: '#00FFFF', location: 1 },
 ];
 
 yellowMagentaTeal = [
   { color: '#FFB90D', location: 0 },
   { color: '#CC0045', location: 0.5 },
   { color: '#009980', location: 1 }
+];
+
+patriot = [
+  { color: '#fff0da', location: 0 },
+  { color: '#00639a', location: 0.111 },
+  { color: '#ca255b', location: 0.222 },
+  { color: '#fff0da', location: 0.333 },
+  { color: '#00639a', location: 0.444 },
+  { color: '#ca255b', location: 0.555 },
+  { color: '#fff0da', location: 0.666 },
+  { color: '#00639a', location: 0.777 },
+  { color: '#ca255b', location: 0.888 },
+  { color: '#fff0da', location: 1 }
+];
+
+chrome = [
+  { color: '#2989cc', location: 0 },
+  { color: '#ffffff', location: 0.5 },
+  { color: '#906a00', location: 0.52 },
+  { color: '#d99f00', location: 0.64 },
+  { color: '#ffffff', location: 1 }
+];
+
+steel = [
+  { color: '#1e5db2', location: 0 },
+  { color: '#e7e9f4', location: 0.25 },
+  { color: '#1e5db2', location: 0.5 },
+  { color: '#e7e9f4', location: 0.75 },
+  { color: '#1e5db2', location: 1 },
+];
+
+mist = [
+  { color: '#7fc99b', location: 0 },
+  { color: '#8aadb4', location: 0.25 },
+  { color: '#938ec5', location: 0.5 },
+  { color: '#87b0d9', location: 0.75 },
+  { color: '#74d0ed', location: 1 },
+];
+
+fire = [
+  { color: '#fff33b', location: 0 },
+  { color: '#fcc60e', location: 0.28 },
+  { color: '#f49040', location: 0.67 },
+  { color: '#ed693c', location: 0.89 },
+  { color: '#ea3e3a', location: 1 },
 ];
