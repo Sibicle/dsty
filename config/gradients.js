fe72e3c3bcea240062a861e26af96785aefa54c3
Jason Nedell <jason@nedell.com>
2015-06-16 21:01:47 -0600

    Start add gradient support

---
+++ config/gradients.js
@@ -0,0 +1,11 @@
+cymk = [
+  { color: '#00FFFF', location: 0 },
+  { color: '#FF00FF', location: 0.5 },
+  { color: '#FFFF00', location: 1 },
+];
+
+yellowMagentaTeal = [
+  { color: '#FFB90D', location: 0 },
+  { color: '#CC0045', location: 0.5 },
+  { color: '#009980', location: 1 }
+];
